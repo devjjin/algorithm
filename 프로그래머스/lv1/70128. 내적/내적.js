aa48d66d12e7a708eda81d655e54a7fbd3715c67
devjjin <38846447+devjjin@users.noreply.github.com>
2023-04-08 08:55:22 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,3 @@
 function solution(a, b) {
-    let answer = 0;
-    for(let i=0; i < a.length; i++){
-        answer += a[i] * b[i];
-    }
-    return answer;
+    return a.reduce((acc, cur, idx) => acc += cur * b[idx], 0);
 }
