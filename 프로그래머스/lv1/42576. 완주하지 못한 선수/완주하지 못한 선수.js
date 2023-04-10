67fbca2c6d041f9250243c8d1db19f69e79c218c
devjjin <38846447+devjjin@users.noreply.github.com>
2023-04-10 10:05:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 30.16 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,8 +1,15 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-
-    for(let i in participant) {
-        if(participant[i] !== completion[i]) return participant[i];
+    const map = new Map();
+    for(let i = 0; i < participant.length; i++) {
+        let a = participant[i], 
+            b = completion[i];
+        map.set(a, (map.get(a) || 0) + 1);
+        map.set(b, (map.get(b) || 0) - 1);
     }
+
+    for(let [k, v] of map) {
+        if(v > 0) return k;
+    }
+
+    return 'nothing';
 }
