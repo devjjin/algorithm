14a30304098b0c43b262e4c6e5374f6285ba8a1e
devjjin <38846447+devjjin@users.noreply.github.com>
2023-04-10 11:08:18 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,10 +1,14 @@
 function solution(s) {
-    let answer = '';
-    let idx = 0;
-    for (let word of s) {
-        answer += (idx % 2 === 0) ? word.toUpperCase() : word.toLowerCase();
-        idx++;
-        if (word === ' ') idx = 0;
+  let answer = '';
+  let idx = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ' ') {
+      idx = 0;
+      answer += ' ';
+    } else {
+      answer += (idx % 2 === 0) ? s[i].toUpperCase() : s[i].toLowerCase();
+      idx++;
     }
-    return answer;
+  }
+  return answer;
 }
