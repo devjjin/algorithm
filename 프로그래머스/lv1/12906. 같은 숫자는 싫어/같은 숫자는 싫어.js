ac7780d45dcda6db5b2254629143f9b0d0a0e68a
devjjin <38846447+devjjin@users.noreply.github.com>
2023-04-12 07:57:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 19.68 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,11 @@
 function solution(arr) {
-    let answer = [arr[0]];
-    for (let i=1; i<arr.length; i++) {
-        if (answer[answer.length - 1] !== arr[i]) {
-            answer.push(arr[i]);
+    const answer = [];
+    let prev = null;
+    arr.forEach((num) => {
+        if(num !== prev) {
+            answer.push(num);
+            prev = num;
         }
-    }
+    })
     return answer;
 }
