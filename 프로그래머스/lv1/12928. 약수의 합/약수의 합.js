1b0124280baa6f3811115cd0660931f9929f76ba
devjjin <38846447+devjjin@users.noreply.github.com>
2023-07-11 22:35:03 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,4 +1,9 @@
 function solution(n) {
-    var answer = 0;
+    let answer = 0;
+    for (let i = 0; i <= n; i++) {
+        if (n % i === 0) { 
+            answer += i;
+        }
+    }
     return answer;
 }
