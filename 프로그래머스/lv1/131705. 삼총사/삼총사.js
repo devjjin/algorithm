65c458ec2f81b3875eb524b593c05a2afb6a6c6c
devjjin <38846447+devjjin@users.noreply.github.com>
2023-07-17 10:07:50 +0900

    [level 1] Title: 삼총사, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,8 +1,8 @@
 function solution(number) {
     let answer = 0;
     let length = number.length;
-    for(let i = 0; i < length; i++) {
-        for(let j = i + 1; j < length; j++) {
+    for(let i = 0; i < length - 2; i++) {
+        for(let j = i + 1; j < length - 1; j++) {
             for(let k = j + 1;  k < length; k++) {
                 if(number[i] + number[j] + number[k] === 0) {
                     answer++;
